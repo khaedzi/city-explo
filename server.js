1871d75ba4fc5d6b9d55369c80bb6b58bb178623
khaedzi <khaled.ziadeh@yahoo.com>
2021-02-21 14:16:34 +0200

    ss

---
+++ server.js
@@ -0,0 +1,9 @@
+// require statements (importing packages)
+let express = require('express');
+// initializations and configuration
+let app = express();
+require('dotenv').config();
+const PORT = process.env.PORT;
+app.listen(PORT, ()=>{
+  console.log('the app is listening on port '+PORT);
+});
